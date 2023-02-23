fc4ad95b0456733ab0e91964f865860d8b9d8521
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-02-23 09:10:09 -0300

    ex rpg

---
+++ js_I_onebitcode/7_rpg.js
@@ -0,0 +1,15 @@
+let damage = 0;
+let name_character = prompt("Digite o nome do seu personagem: ");
+let attack_power_character = prompt(
+  "Digite o poder de ataque de seu personagem: "
+);
+let name_character_opponent = prompt(
+  "Digite o nome do personagem adversário: "
+);
+let life_opponent = prompt("Digite quantos pontos de vida tem seu adversário:");
+let defense_opponent = prompt("Digite o poder de defesa do seu adversário:");
+let shield_opponent = prompt("O seu adversário possui escudo? (n/s)");
+
+if (attack_power_character > defense_opponent) and(shield_opponent == "n"){
+  
+}
