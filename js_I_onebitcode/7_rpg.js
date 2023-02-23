03ccfb64a9de38ddc8db66ce688ce90f60489b19
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-02-23 11:14:11 -0300

    switch

--- js_I_onebitcode/7_rpg.js
+++ js_I_onebitcode/7_rpg.js
@@ -1,16 +1,43 @@
-let damage = 0;
-let name_character = prompt("Digite o nome do seu personagem: ");
-let attack_power_character = prompt(
+const name_character = prompt("Digite o nome do seu personagem: ");
+const attack_power_character = prompt(
   "Digite o poder de ataque de seu personagem: "
 );
-let name_character_opponent = prompt(
+const name_character_opponent = prompt(
   "Digite o nome do personagem adversário: "
 );
 let life_opponent = prompt("Digite quantos pontos de vida tem seu adversário:");
 let defense_opponent = prompt("Digite o poder de defesa do seu adversário:");
 let shield_opponent = prompt("O seu adversário possui escudo? (s/n)");
 
+let damage = 0;
 if (attack_power_character > defense_opponent && shield_opponent === "n") {
   damage = attack_power_character - defense_opponent;
+} else if (
+  attack_power_character > defense_opponent &&
+  shield_opponent === "s"
+) {
+  damage = (attack_power_character - defense_opponent) / 2;
 }
-alert(damage);
+
+life_opponent = life_opponent - damage;
+
+alert(
+  name_character +
+    " causou " +
+    damage +
+    " pontos de dano em " +
+    name_character_opponent
+);
+alert(
+  name_character +
+    "\nPoder de ataque: " +
+    attack_power_character +
+    "\n\n" +
+    name_character_opponent +
+    "\nPontos de vida: " +
+    life_opponent +
+    "\nPoder de defesa: " +
+    defense_opponent +
+    "\nPossui escudo: " +
+    shield_opponent
+);
