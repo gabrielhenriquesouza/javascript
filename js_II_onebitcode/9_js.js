307172c36c2d90edbed7d154f88e2cf7baf31c1d
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-19 10:26:24 -0300

    dom

--- js_II_onebitcode/9_js.js
+++ js_II_onebitcode/9_js.js
@@ -1,3 +1,28 @@
-function addTechnologies{
-  
-}
+/*function addTechnologies() {
+  let technologies = document.getElementById("technologies");
+  console.log(technologies);
+
+  let labelName = document.createElement("label");
+
+  let technologyName = document.createElement("input");
+  technologyName.type = "text";
+  technologyName.id = "technologyName";
+  technologyName.name = "technologyName";
+
+  technologies.appendChild(technologyName);
+}*/
+
+let buttonAddTechnology = document.getElementById("addTechnology");
+
+buttonAddTechnology.addEventListener("click", function (ev) {
+  ev.preventDefault();
+
+  let technologies = document.getElementById("technologies");
+
+  let inputTechnologyName = document.createElement("input");
+  technologyName.type = "text";
+  technologyName.id = "technologyName";
+  technologyName.name = "technologyName";
+
+  technologies.appendChild(inputTechnologyName);
+});
