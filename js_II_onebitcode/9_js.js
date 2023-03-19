00ac71c1454f6f165e45939c42c12bf848367fc0
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-19 11:27:07 -0300

    dom

--- js_II_onebitcode/9_js.js
+++ js_II_onebitcode/9_js.js
@@ -5,13 +5,39 @@ buttonAddTechnology.addEventListener("click", function (ev) {
 
   let form = document.getElementById("technologies");
 
+  let ul = document.createElement("ul");
+  let li = document.createElement("li");
+  li.innerText = "Nome: ";
+  let formTechnologies = document.createElement("form");
+
   let inputTechnologyName = document.createElement("input");
   inputTechnologyName.type = "text";
   inputTechnologyName.id = "technologyName";
   inputTechnologyName.name = "technologyName";
 
-  let yearsExperience = document.createElement("input");
-  yearsExperience.type = "radio";
+  let yearsExperience0_2 = document.createElement("input");
+  yearsExperience0_2.type = "radio";
+  yearsExperience0_2.name = "yearsExperience";
 
-  form.appendChild(inputTechnologyName);
+  let yearsExperience3_4 = document.createElement("input");
+  yearsExperience3_4.type = "radio";
+  yearsExperience3_4.name = "yearsExperience";
+
+  let yearsExperience5 = document.createElement("input");
+  yearsExperience5.type = "radio";
+  yearsExperience5.name = "yearsExperience";
+
+  formTechnologies.appendChild(inputTechnologyName);
+  formTechnologies.appendChild(yearsExperience0_2);
+  formTechnologies.appendChild(yearsExperience3_4);
+  formTechnologies.appendChild(yearsExperience5);
+  li.appendChild(formTechnologies);
+  ul.appendChild(li);
+  form.appendChild(ul);
 });
+
+/*
+    <input type="radio" name="radio" id="" value="A" />
+    <input type="radio" name="radio" id="" value="B" />
+    <input type="radio" name="radio" id="" value="C" />
+*/
