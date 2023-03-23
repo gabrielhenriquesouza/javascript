1358f30d9c7b5eb66bb866f802589cd6bb780211
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-23 11:41:33 -0300

    dom

--- js_II_onebitcode/11_atributos.js
+++ js_II_onebitcode/11_atributos.js
@@ -1,5 +1,30 @@
 let input = document.getElementById("input");
 
 document.getElementById("value").addEventListener("click", function () {
-  input.value = input.value === "" ? "Valor alterado" : "";
+  input.value = "Valor alterado";
+
+  console.log(input.value);
+  console.log(input.getAttribute("value"));
+});
+
+document.getElementById("type").addEventListener("click", function () {
+  input.type = input.type !== "radio" ? "radio" : "text";
+  //input.setAttribute("type", "radio");
+});
+
+document.getElementById("placeholder").addEventListener("click", function () {
+  input.placeholder = "Digite algo...";
+});
+
+document.getElementById("disable").addEventListener("click", function () {
+  input.setAttribute("disabled", !input.disabled);
+});
+
+document.getElementById("data").addEventListener("click", function () {
+  const data = input.dataset.somethingElse;
+  console.log("O valor do atributo data-something é: " + data);
+  input.dataset.somethingElse = "Algum outro valor";
+  console.log(
+    "O valor do atributo data-something é: " + input.dataset.somethingElse
+  );
 });
