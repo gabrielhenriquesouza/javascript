6c4ebadd759eea9f19a79358958583e9faeca932
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-23 14:36:08 -0300

    projeto calculador de IMC

---
+++ pratica/1_calculadora_imc.js
@@ -0,0 +1,28 @@
+/*
+        <h1 class="titleCalculator">Calculadora - IMC</h1>
+        <hr />
+        <form class="formCalculator">
+          <label for="name" class="calculatorLabel">Nome:</label>
+          <input type="text" name="name" id="name" class="calculatorInput" />
+          <br />
+          <label for="height" class="calculatorLabel">Altura:</label>
+          <input
+            type="number"
+            name="height"
+            id="height"
+            class="calculatorInput"
+          />
+          <br />
+          <label for="weight" class="calculatorLabel">Peso:</label>
+          <input
+            type="number"
+            name="weight"
+            id="weight"
+            class="calculatorInput"
+          />
+          <br />
+          <button class="calculateButton">Calcular</button>
+          <br />
+          <div class="result"></div>
+        </form> 
+*/
