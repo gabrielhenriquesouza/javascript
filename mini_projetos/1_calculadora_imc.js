7bd1dbc7b4f59cda75e5884c34ccda1d94ecfe14
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-24 14:26:52 -0300

    calculador de IMC

--- mini_projetos/1_calculadora_imc.js
+++ mini_projetos/1_calculadora_imc.js
@@ -1,27 +1,36 @@
+let result = document.getElementById("result");
+
 document
   .getElementById("calculateBtn")
   .addEventListener("click", function (ev) {
     ev.preventDefault();
+    if (result != "") {
+    }
 
     const personsName = document.getElementById("name").value;
     const height = document.getElementById("height").value;
     const weight = document.getElementById("weight").value;
-    const result = document.getElementById("result");
 
-    const imc = weight / (height * height);
-
+    let imc = weight / (height * height);
+    let rel = "";
     if (imc < 18.5) {
+      rel = "está abaixo do peso!";
     } else if (imc >= 18.5 && imc <= 24.9) {
+      rel = "está no peso ideal!";
     } else if (imc >= 25 && imc <= 29.9) {
+      rel = "está acima do peso!";
     } else if (imc >= 30 && imc <= 39.9) {
+      rel = "está com obesidade!";
     } else if (imc >= 40) {
+      rel = "está com obesidade grave!";
     }
+
+    const resultTxt = personsName + " seu IMC é " + imc + " e você " + rel;
+    result.append(resultTxt);
   });
 
-/*
-  MENOR QUE 18,5	MAGREZA	0
-  ENTRE 18,5 E 24,9	NORMAL	0
-  ENTRE 25,0 E 29,9	SOBREPESO	I
-  ENTRE 30,0 E 39,9	OBESIDADE	II
-  MAIOR QUE 40,0	OBESIDADE GRAVE	
-   */
+/*MENOR QUE 18,5	MAGREZA	0
+ENTRE 18,5 E 24,9	NORMAL	0
+ENTRE 25,0 E 29,9	SOBREPESO	I
+ENTRE 30,0 E 39,9	OBESIDADE	II
+MAIOR QUE 40,0	OBESIDADE GRAVE	 */
