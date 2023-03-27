4d46f01b8f4087ac777a26d0967748266677a98b
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-27 10:34:24 -0300

    jogo da velha

--- js_II_onebitcode/13.jogo_da_velha/13_jogo_da_velha.js
+++ js_II_onebitcode/13.jogo_da_velha/13_jogo_da_velha.js
@@ -4,15 +4,12 @@ let nameO = document.getElementById("o");
 let arr = [[], [], []];
 for (let i = 1; i <= 3; i++) {
   arr[0].push(document.getElementsByClassName("item" + i));
-  console.log(arr[0][i]);
 }
-for (let o = 1; o <= 3; o++) {
+for (let o = 4; o <= 6; o++) {
   arr[1].push(document.getElementsByClassName("item" + o));
-  console.log(arr[1][o]);
 }
-for (let p = 1; p <= 3; p++) {
+for (let p = 7; p <= 9; p++) {
   arr[2].push(document.getElementsByClassName("item" + p));
-  console.log(arr[2][p]);
 }
 
 console.table(arr);
