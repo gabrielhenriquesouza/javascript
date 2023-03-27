0e4f2ff5330f3f66f9a84732a58f63520f37b25d
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-27 10:27:08 -0300

    jogo da velha

--- js_II_onebitcode/13.jogo_da_velha/13_jogo_da_velha.js
+++ js_II_onebitcode/13.jogo_da_velha/13_jogo_da_velha.js
@@ -1,9 +1,21 @@
 let nameX = document.getElementById("x");
 let nameO = document.getElementById("o");
 
-let arr = [];
-for (let i = 0; i <= 8; i++) {
-  arr.push(document.getElementById("item" + i));
+let arr = [[], [], []];
+for (let i = 1; i <= 3; i++) {
+  arr[0].push(document.getElementsByClassName("item" + i));
+  console.log(arr[0][i]);
+}
+for (let o = 1; o <= 3; o++) {
+  arr[1].push(document.getElementsByClassName("item" + o));
+  console.log(arr[1][o]);
+}
+for (let p = 1; p <= 3; p++) {
+  arr[2].push(document.getElementsByClassName("item" + p));
+  console.log(arr[2][p]);
 }
 
-console.log(nameX, nameO, arr);
+console.table(arr);
+//document.getElementById("item1").addEventListener("click", function () {
+//  alert("oi");
+//});
