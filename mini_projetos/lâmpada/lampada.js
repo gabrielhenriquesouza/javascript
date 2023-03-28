c81cc6fc26683e81a041ab4b72b796ea120a2bbb
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-28 14:30:05 -0300

    projeto lampada

--- mini_projetos/lâmpada/lampada.js
+++ mini_projetos/lâmpada/lampada.js
@@ -1,27 +1,24 @@
 //Botão para ligar a lampada
-document.getElementById("lightUp").addEventListener(" click", function (ev) {
-  ev.preventDefault();
-
+document.getElementById("lightUp").addEventListener("click", function (ev) {
   document.getElementById("img").src = "imagens/ligada.jpg";
 });
 
 //Botão para desligar a lampada
 document.getElementById("toSwitchOff").addEventListener("click", function (ev) {
-  ev.preventDefault();
-
   document.getElementById("img").src = "imagens/desligada.jpg";
 });
 
-//Quando o mouse estiver em cima a lampada acende
+//Quando o mouse estiver em cima a lampada liga
 document.getElementById("img").addEventListener("mouseover", function (ev) {
-  ev.preventDefault();
-
   this.src = "imagens/ligada.jpg";
 });
 
-//Quando o mouse sair a lampada apaga
+//Quando o mouse sair a lampada desliga
 document.getElementById("img").addEventListener("mouseout", function (ev) {
-  ev.preventDefault();
-
   this.src = "imagens/desligada.jpg";
+});
+
+//Duplo click quebra a lampada
+document.getElementById("img").addEventListener("click", function (ev) {
+  this.src = "imagens/quebrada.jpg";
 });
