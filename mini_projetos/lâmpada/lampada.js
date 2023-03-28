47bb21f04667b372b4d18839d2f1492ed64dbb1b
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-28 14:08:34 -0300

    projeto lampada

--- mini_projetos/lâmpada/lampada.js
+++ mini_projetos/lâmpada/lampada.js
@@ -1,13 +1,27 @@
-document.getElementById("lightUp").addEventListener("click", function (ev) {
+//Botão para ligar a lampada
+document.getElementById("lightUp").addEventListener(" click", function (ev) {
   ev.preventDefault();
 
   document.getElementById("img").src = "imagens/ligada.jpg";
 });
 
+//Botão para desligar a lampada
 document.getElementById("toSwitchOff").addEventListener("click", function (ev) {
   ev.preventDefault();
+
+  document.getElementById("img").src = "imagens/desligada.jpg";
 });
 
-/*
-document.getElementById(img).src =”novaImagem.png”;
- */
+//Quando o mouse estiver em cima a lampada acende
+document.getElementById("img").addEventListener("mouseover", function (ev) {
+  ev.preventDefault();
+
+  this.src = "imagens/ligada.jpg";
+});
+
+//Quando o mouse sair a lampada apaga
+document.getElementById("img").addEventListener("mouseout", function (ev) {
+  ev.preventDefault();
+
+  this.src = "imagens/desligada.jpg";
+});
