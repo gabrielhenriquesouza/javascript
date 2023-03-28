971facbb5252dd2e581a9a06de7615c592d4ccf0
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-28 13:29:26 -0300

    projeto lampada

---
+++ mini_projetos/lâmpada/lampada.js
@@ -0,0 +1,13 @@
+document.getElementById("lightUp").addEventListener("click", function (ev) {
+  ev.preventDefault();
+
+  document.getElementById("img").src = "imagens/ligada.jpg";
+});
+
+document.getElementById("toSwitchOff").addEventListener("click", function (ev) {
+  ev.preventDefault();
+});
+
+/*
+document.getElementById(img).src =”novaImagem.png”;
+ */
