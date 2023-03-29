3ab9900363ea72eba029203272a3f4962a7a1f92
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-29 13:36:04 -0300

    projeto lampada

--- mini_projetos/lâmpada/lampada.js
+++ mini_projetos/lâmpada/lampada.js
@@ -1,38 +1,41 @@
+let img = document.getElementById("img");
+
 //Botão para ligar a lampada
-document.getElementById("lightUp").addEventListener("click", function () {
-  document.getElementById("img").src = "imagens/ligada.jpg";
-  document.getElementById("img").alt = "lampada_ligada";
-});
+document
+  .getElementById("turnOnAndTurnOff")
+  .addEventListener("click", function () {
+    if ((img.src = "imagens/desligada.jpg")) {
+      img.src = "imagens/ligada.jpg";
+    } else if ((img.src = "imagens/ligada.jpg")) {
+      img.src = "imagens/desligada.jpg";
+    }
+  });
 
 //Botão para desligar a lampada
-document.getElementById("toSwitchOff").addEventListener("click", function () {
-  document.getElementById("img").src = "imagens/desligada.jpg";
-  document.getElementById("img").alt = "lampada_desligada";
-});
 
 //Quando o mouse estiver em cima a lampada liga
-document.getElementById("img").addEventListener("mouseover", function () {
-  this.src = "imagens/ligada.jpg";
-  this.alt = "lampada_ligada";
+/*
+img.addEventListener("mouseover", function () {
+  img.src = "imagens/ligada.jpg";
+  img.alt = "lampada_ligada";
 });
 
 //Quando o mouse sair a lampada desliga
-document.getElementById("img").addEventListener("mouseout", function () {
-  this.src = "imagens/desligada.jpg";
-  this.alt = "lampada_desligada";
+img.addEventListener("mouseout", function () {
+  img.src = "imagens/desligada.jpg";
+  img.alt = "lampada_desligada";
 });
 
 //Duplo click quebra a lampada
-document.getElementById("img").addEventListener("dblclick", function () {
+img.addEventListener("dblclick", function () {
   let result = document.getElementById("result");
-  let img = document.getElementById("img");
+
   result.removeChild(img);
 
-  img = document.createElement("img");
-  img.src = "imagens/quebrada.jpg";
-  img.id = "brokenImage";
+  let imgBroken = document.createElement("img");
+  imgBroken.src = "imagens/quebrada.jpg";
+  imgBroken.id = "brokenImage";
 
-  result.appendChild(img);
-  //this.src = "imagens/quebrada.jpg";
-  //this.alt = "lampada_quebrada";
+  result.appendChild(imgBroken);
 });
+*/
