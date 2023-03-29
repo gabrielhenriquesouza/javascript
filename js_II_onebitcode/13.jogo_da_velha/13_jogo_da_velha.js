a9320265c0f8c8788c0670f515a508236d89cde8
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-03-29 10:03:35 -0300

    jogo da velha

--- js_II_onebitcode/13.jogo_da_velha/13_jogo_da_velha.js
+++ js_II_onebitcode/13.jogo_da_velha/13_jogo_da_velha.js
@@ -1,149 +1,184 @@
 function initializeGame() {
-  player1Move();
+  checkButtonClickedPlayer1();
+  checkButtonClickedPlayer2();
 }
 
 function checkButtonClickedPlayer1() {
-  console.log("entrei na verificação do button");
-
+  let verification = false;
   document.getElementById("item1").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "x";
-      this.dataset.value = "x";
-      player2Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "x";
+        this.dataset.value = "x";
+        return (verification = true);
+      }
     }
   });
+
   document.getElementById("item2").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "x";
-      this.dataset.value = "x";
-      player2Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "x";
+        this.dataset.value = "x";
+        return (verification = true);
+      }
     }
   });
+
   document.getElementById("item3").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "x";
-      this.dataset.value = "x";
-      player2Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "x";
+        this.dataset.value = "x";
+        return (verification = true);
+      }
     }
   });
+
   document.getElementById("item4").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "x";
-      this.dataset.value = "x";
-      player2Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "x";
+        this.dataset.value = "x";
+        return (verification = true);
+      }
     }
   });
+
   document.getElementById("item5").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "x";
-      this.dataset.value = "x";
-      player2Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "x";
+        this.dataset.value = "x";
+        return (verification = true);
+      }
     }
   });
+
   document.getElementById("item6").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "x";
-      this.dataset.value = "x";
-      player2Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "x";
+        this.dataset.value = "x";
+        return (verification = true);
+      }
     }
   });
+
   document.getElementById("item7").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "x";
-      this.dataset.value = "x";
-      player2Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "x";
+        this.dataset.value = "x";
+        return (verification = true);
+      }
     }
   });
+
   document.getElementById("item8").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "x";
-      this.dataset.value = "x";
-      player2Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "x";
+        this.dataset.value = "x";
+        return (verification = true);
+      }
     }
   });
+
   document.getElementById("item9").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "x";
-      this.dataset.value = "x";
-      player2Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "x";
+        this.dataset.value = "x";
+        return (verification = true);
+      }
     }
   });
 }
 
 function checkButtonClickedPlayer2() {
+  let verification = false;
   document.getElementById("item1").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "o";
-      this.dataset.value = "o";
-      player1Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "o";
+        this.dataset.value = "o";
+        return (verification = true);
+      }
     }
   });
   document.getElementById("item2").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "o";
-      this.dataset.value = "o";
-      player1Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "o";
+        this.dataset.value = "o";
+        return (verification = true);
+      }
     }
   });
   document.getElementById("item3").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "o";
-      this.dataset.value = "o";
-      player1Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "o";
+        this.dataset.value = "o";
+        return (verification = true);
+      }
     }
   });
   document.getElementById("item4").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "o";
-      this.dataset.value = "o";
-      player1Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "o";
+        this.dataset.value = "o";
+        return (verification = true);
+      }
     }
   });
   document.getElementById("item5").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "o";
-      this.dataset.value = "o";
-      player1Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "o";
+        this.dataset.value = "o";
+        return (verification = true);
+      }
     }
   });
   document.getElementById("item6").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "o";
-      this.dataset.value = "o";
-      player1Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "o";
+        this.dataset.value = "o";
+        return (verification = true);
+      }
     }
   });
   document.getElementById("item7").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "o";
-      this.dataset.value = "o";
-      player1Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "o";
+        this.dataset.value = "o";
+        return (verification = true);
+      }
     }
   });
   document.getElementById("item8").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "o";
-      this.dataset.value = "o";
-      player1Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "o";
+        this.dataset.value = "o";
+        return (verification = true);
+      }
     }
   });
   document.getElementById("item9").addEventListener("click", function () {
-    if (this.dataset.value === "-") {
-      this.innerText = "o";
-      this.dataset.value = "o";
-      player1Move();
+    if (verification === false) {
+      if (this.dataset.value === "-") {
+        this.innerText = "o";
+        this.dataset.value = "o";
+        return (verification = true);
+      }
     }
   });
-}
-
-function player1Move() {
-  alert("JOGADOR 1");
-  checkButtonClickedPlayer1();
-}
-
-function player2Move() {
-  alert("JOGADOR 2");
-  checkButtonClickedPlayer2();
 }
 
 let nameX = document.getElementById("x");
