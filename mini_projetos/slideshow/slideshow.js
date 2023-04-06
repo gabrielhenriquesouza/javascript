bffefa4260ddf2630146ef69cc5caff0ed1e420d
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-04-06 13:33:54 -0300

    projeto slideshow

---
+++ mini_projetos/slideshow/slideshow.js
@@ -0,0 +1,12 @@
+let img = document.getElementById("img");
+document.getElementById("next").addEventListener("click", function () {
+  if (img.alt == "yuyuhakusho") {
+  }
+});
+
+/*
+Ordem alts
+yuyuhakusho
+chrono
+inuyasha
+ */
