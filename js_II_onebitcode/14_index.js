193dd72c7d0ff07b5ae80233c2ccf8aa55fdf639
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-04-06 10:30:45 -0300

    armazenamento

--- js_II_onebitcode/14_index.js
+++ js_II_onebitcode/14_index.js
@@ -4,4 +4,30 @@ document.getElementById("sessionBtn").addEventListener("click", function () {
   sessionStorage.setItem("info: ", input.value);
   input.value = "";
 });
-document.getElementById("read");
+document.getElementById("readSession").addEventListener("click", function () {
+  let info = sessionStorage.getItem("info: ");
+  alert("A informação salva é: " + info);
+});
+
+//Local storage
+document.getElementById("localBtn").addEventListener("click", function () {
+  let input = document.getElementById("local");
+  localStorage.setItem("localInfo", input.value);
+  input.value = "";
+});
+document.getElementById("readLocal").addEventListener("click", function () {
+  let info = localStorage.getItem("localInfo");
+  alert("A informação salva no local é: " + info);
+});
+
+//Cookies
+document.getElementById("cookieBtn").addEventListener("click", function () {
+  let input = document.getElementById("cookie");
+  // cookieName=value; expires=UTCStringDate; path=/;
+  const cookie = "info=" + input.value + ";";
+  const expiration = "expires=" + new Date(2023, 4, 7) + ";";
+  const path = "path=/;";
+  document.cookie = cookie + expiration + path;
+  input.value = "";
+  console.log(document.cookie);
+});
