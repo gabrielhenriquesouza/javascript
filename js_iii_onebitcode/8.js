85ae9a40106c0dbd2318db5fc668faa4a77a8227
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-04-10 11:05:29 -0300

    estatística

--- js_iii_onebitcode/8.js
+++ js_iii_onebitcode/8.js
@@ -16,6 +16,22 @@ let SimpleAverage = (...numbers) => {
 };
 SimpleAverage(8, 6.2, 7);
 
-let weightedAverage =;
+let weightedAverage = (...objects) => {
+  let sum = 0;
+  let pValue = 0;
 
-weightedAverage({ n: 7, p: 2}, { n: 9, p: 5 }, { n: 3, p: 1 })
+  for (let i = 0; i < objects.length; i++) {
+    let multiplication = objects[i].n * objects[i].p;
+
+    sum += multiplication;
+
+    pValue += objects[i].p;
+  }
+
+  let average = sum / pValue;
+
+  console.log(`Calculo de Média Aritmética Ponderada
+  `);
+};
+
+weightedAverage({ n: 7, p: 2 }, { n: 9, p: 5 }, { n: 3, p: 1 });
