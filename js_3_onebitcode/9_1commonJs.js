96330061d7dbd48d01e8786b98954170ec6afa71
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-04-13 10:00:47 -0300

    common modules

---
+++ js_3_onebitcode/9_1commonJs.js
@@ -0,0 +1,7 @@
+const render = require("./9_2render_commonJs");
+const store = require("./9_2storeJs");
+
+console.log("Aplicação iniciada.");
+render();
+store();
+console.log("Aplicação finalizada.");
