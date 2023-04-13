cc61eed82e68dc72489d10348ea87e181ce84e90
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-04-13 10:26:01 -0300

    Es Modules

---
+++ js_3_onebitcode/10_index.js
@@ -0,0 +1,11 @@
+import { label, input, br } from "./10_functions.js";
+
+console.log(label({ for: "fullname", textContent: "Nome Completo" }));
+console.log(
+  input({
+    id: "fullname",
+    name: "fullname",
+    placeholder: "Digite seu nome completo...",
+  })
+);
+console.log(br());
