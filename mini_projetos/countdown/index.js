c90e55b666819c427a5d0d8a5a587260888ff785
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-04-25 13:56:56 -0300

    projeto countdown

--- mini_projetos/countdown/index.js
+++ mini_projetos/countdown/index.js
@@ -1,10 +1,30 @@
 function updateTime() {
-  if (seconds.innerText != 0) {
-    seconds.innerText = seconds.innerText - 1;
+  if (
+    seconds.innerText == minutes.innerText &&
+    minutes.innerText == hours.innerText &&
+    hours.innerText == days.innerText
+  ) {
   } else {
-    seconds.innerText = 59;
-    if (minutes.innerText != 0) {
-      minutes.innerText = minutes.innerText - 1;
+    if (seconds.innerText != 0) {
+      seconds.innerText = seconds.innerText - 1;
+    } else {
+      seconds.innerText = 59;
+
+      if (minutes.innerText != 0) {
+        minutes.innerText = minutes.innerText - 1;
+      } else {
+        minutes.innerText = 59;
+
+        if (hours.innerText != 0) {
+          hours.innerText = hours.innerText - 1;
+        } else {
+          hours.innerText = 23;
+
+          if (days.innerText != 0) {
+            days.innerText = days.innerText - 1;
+          }
+        }
+      }
     }
   }
 }
@@ -14,8 +34,9 @@ let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 
-days.innerText = 03;
+days.innerText = 0;
 hours.innerText = 0;
-minutes.innerText = 1;
+minutes.innerText = 0;
 seconds.innerText = 10;
+
 setInterval(updateTime, 1000);
