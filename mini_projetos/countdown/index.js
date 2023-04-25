a3c499089b190fe0c9859768a8d55845f9f6daa3
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-04-25 14:18:22 -0300

    projeto countdown

--- mini_projetos/countdown/index.js
+++ mini_projetos/countdown/index.js
@@ -1,27 +1,30 @@
+const formatarDigito = (digito) => `0${digito}`.slice(-2);
+
 function updateTime() {
   if (
     seconds.innerText == minutes.innerText &&
     minutes.innerText == hours.innerText &&
     hours.innerText == days.innerText
   ) {
+    clearInterval(interval);
   } else {
     if (seconds.innerText != 0) {
-      seconds.innerText = seconds.innerText - 1;
+      seconds.innerText = formatarDigito(seconds.innerText - 1);
     } else {
       seconds.innerText = 59;
 
       if (minutes.innerText != 0) {
-        minutes.innerText = minutes.innerText - 1;
+        minutes.innerText = formatarDigito(minutes.innerText - 1);
       } else {
         minutes.innerText = 59;
 
         if (hours.innerText != 0) {
-          hours.innerText = hours.innerText - 1;
+          hours.innerText = formatarDigito(hours.innerText - 1);
         } else {
           hours.innerText = 23;
 
           if (days.innerText != 0) {
-            days.innerText = days.innerText - 1;
+            days.innerText = formatarDigito(days.innerText - 1);
           }
         }
       }
@@ -34,9 +37,9 @@ let hours = document.getElementById("hours");
 let minutes = document.getElementById("minutes");
 let seconds = document.getElementById("seconds");
 
-days.innerText = 3;
-hours.innerText = 0;
-minutes.innerText = 0;
+days.innerText = formatarDigito(3);
+hours.innerText = formatarDigito(7);
+minutes.innerText = formatarDigito(12);
 seconds.innerText = 10;
 
-setInterval(updateTime, 1000);
+let interval = setInterval(updateTime, 1000);
