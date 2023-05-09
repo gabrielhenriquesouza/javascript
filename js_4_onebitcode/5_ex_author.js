27bcdd3c72a276658766ce02e123adbee91f642d
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-05-09 11:28:06 -0300

    ex associação POO

--- js_4_onebitcode/5_ex_author.js
+++ js_4_onebitcode/5_ex_author.js
@@ -3,5 +3,14 @@ const Post = require("./5_ex_post");
 class Author {
   constructor(name) {
     this.name = name;
+    this.posts = [];
+  }
+
+  writePost(title, body) {
+    const post = new Post(title, body, this);
+    this.posts.push(post);
+    return post;
   }
 }
+
+module.exports = Author;
