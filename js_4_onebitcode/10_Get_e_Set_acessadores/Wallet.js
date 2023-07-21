ddf7217ae3b5f34d8748741aed84dc76ed2cf86c
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-07-21 10:44:35 -0300

    acessadores get e set

--- js_4_onebitcode/10_Get_e_Set_acessadores/Wallet.js
+++ js_4_onebitcode/10_Get_e_Set_acessadores/Wallet.js
@@ -1,5 +1,6 @@
 class Wallet {
   #amount;
+  #username;
 
   constructor() {
     this.#amount = 100.99 * 100;
@@ -8,7 +9,22 @@ class Wallet {
   get amount() {
     return this.#amount / 100;
   }
+
+  set username(newUserName) {
+    if (typeof newUserName === "string") {
+      this.#username = newUserName;
+    } else {
+      console.log("Username must be of type string");
+    }
+  }
+
+  get username() {
+    return this.#username;
+  }
 }
 
 const myWallet = new Wallet();
 console.log(myWallet.amount);
+myWallet.username = "Gabriel Henrique";
+myWallet.username = 2;
+console.log(myWallet.username);
