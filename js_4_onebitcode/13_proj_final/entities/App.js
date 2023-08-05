f25d1396398ef0074ac0b24a5450dbf601c27916
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-08-05 11:00:17 -0300

    proj poo

--- js_4_onebitcode/13_proj_final/entities/App.js
+++ js_4_onebitcode/13_proj_final/entities/App.js
@@ -1,12 +1,17 @@
 class App {
-  static #listaDeUsuarios = ["br@gmail", "hue@gmail", "123@gmail"];
+  #listaDeUsuarios = ["br@gmail", "hue@gmail", "123@gmail"];
 
   static criarUsuario(email) {
     if (email) {
     }
+  }
 
-    for (let i = 0; i < 20; i++) {
-      console.log(i);
-    }
+   pegarListaDeUsuarios() { // <-- transformar em static
+    let cloneArray = this.#listaDeUsuarios.slice();
+    return cloneArray.length;
   }
 }
+
+
+const instacia = new App();
+console.log(instacia.pegarListaDeUsuarios());
