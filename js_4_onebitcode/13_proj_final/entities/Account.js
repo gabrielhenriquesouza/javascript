e53eefea18cf4ccb790380e344bf8f74bea78531
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-08-16 10:30:00 -0300

    proj poo

--- js_4_onebitcode/13_proj_final/entities/Account.js
+++ js_4_onebitcode/13_proj_final/entities/Account.js
@@ -1,78 +1,35 @@
-const Deposit = require("./Deposit.js");
-const Loan = require("./Loan.js");
-
 module.exports = class Account {
-  #saldo = 0;
+  #balance;
 
-  constructor(email) {
-    this.todosDepositos = [];
-    this.todosEmprestimos = [];
-    this.todasTranferencias = [];
-    this.email = email;
+  constructor(user) {
+    this.owner = user;
+    this.#balance = 0;
+    this.deposits = [];
+    this.loans = [];
+    this.transfers = [];
   }
 
-  dataDeCriacao() {
-    let dataOriginal = new Date();
-    var dia = dataOriginal.getDate();
-    var mes = dataOriginal.getMonth() + 1;
-    var ano = dataOriginal.getFullYear();
-    return (
-      (dia < 10 ? "0" : "") +
-      dia +
-      "/" +
-      (mes < 10 ? "0" : "") +
-      mes +
-      "/" +
-      ano
-    );
+  get balance() {
+    return this.#balance;
   }
 
-  efetuarDeposito(valor) {
-    let dataDeCriacao = this.dataDeCriacao();
-    if (typeof valor === "number" && valor > 0) {
-      this.#saldo += valor;
-      const deposito = new Deposit(valor, dataDeCriacao);
-      this.todosDepositos.push(deposito);
+  addDeposit(deposit) {
+    this.#balance += deposit.value;
+    this.deposits.push(deposit);
+  }
+
+  addLoan(loan) {
+    this.#balance += loan.value;
+    this.loans.push(loan);
+  }
+
+  addTransfer(transfer) {
+    if (transfer.toUser.email === this.owner.email) {
+      this.#balance += transfer.value;
+      this.transfers.push(transfer);
+    } else if (transfer.fromUser.email === this.owner.email) {
+      this.#balance -= transfer.value;
+      this.transfers.push(transfer);
     }
-  }
-
-  emprestimo(valor, dataDeCriacao, numeroDeParcelas) {
-    if (typeof valor === "number" && valor > 0 && numeroDeParcelas > 0) {
-      this.#saldo += valor;
-      const emprestimo = new Loan(valor, dataDeCriacao, numeroDeParcelas);
-      this.todosEmprestimos.push(emprestimo);
-    }
-  }
-
-  transferencia(userRemetente, userDestinatario, valor) {
-    //posso usar email
-    if (userRemetente === this.email && this.#saldo >= valor) {
-      this.#saldo -= valor;
-      this.todasTranferencias.push(
-        `-${valor} transferido para ${userDestinatario}`
-      );
-    } else if (userRemetente !== this.email) {
-      this.#saldo += valor;
-      this.todasTranferencias.push(
-        `+${valor} transferido para ${userDestinatario}`
-      );
-    }
-  }
-
-  aumentarSaldo(valor) {
-    this.#saldo += valor;
-  }
-
-  diminuirSaldo(valor) {
-    this.#saldo -= valor;
-  }
-
-  mostrarSaldo() {
-    return console.log(
-      this.#saldo,
-      this.todosDepositos,
-      this.todosEmprestimos,
-      this.todasTranferencias
-    );
   }
 };
--- js_4_onebitcode/13_proj_final/entities/User.js
+++ js_4_onebitcode/13_proj_final/entities/User.js
@@ -1,9 +1,9 @@
 const Account = require("./Account");
 
 module.exports = class User {
-  constructor(email, nomeCompleto) {
+  constructor(email, fullname) {
     this.email = email;
-    this.nomeCompleto = nomeCompleto;
-    this.conta = new Account(this);
+    this.fullname = fullname;
+    this.account = new Account(this);
   }
 };
