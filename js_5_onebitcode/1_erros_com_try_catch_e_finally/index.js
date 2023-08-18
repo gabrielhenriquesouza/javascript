b36c3d90e5cf76e21b25d5c4dc8b45ea28a37ea9
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-08-18 10:37:21 -0300

    tratando erros

--- js_5_onebitcode/1_erros_com_try_catch_e_finally/index.js
+++ js_5_onebitcode/1_erros_com_try_catch_e_finally/index.js
@@ -3,15 +3,24 @@ function sum(a, b) {
   const secondNumber = Number(b);
 
   if (isNaN(firstNumber) || isNaN(secondNumber)) {
-    throw new Error("arguments must be two numbers");
+    throw new Error("argumentos devem possuir dois números!");
   }
 
   return firstNumber + secondNumber;
 }
 
-console.log(sum(2, 9));
-console.log(sum(true, 14));
-console.log(sum(undefined, 22));
-console.log(sum(18, "0"));
-console.log(sum(39, null));
-console.log(sum(13, "zero"));
+try {
+  console.log(sum(2, 9));
+  console.log(sum(true, 14));
+  //console.log(sum(undefined, 22));
+  console.log(sum(18, "0"));
+  console.log(sum(39, null));
+  //console.log(sum(13, "zero"));
+} catch (error) {
+  console.log("Um erro ocorreu!");
+  console.log(error.message);
+} finally {
+  console.log("Cálculos finalizados!");
+}
+
+console.log("Olá, mundo!");
