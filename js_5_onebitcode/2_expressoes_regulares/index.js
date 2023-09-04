499db8985088355809ad09c7b9b2036633c8b358
gabrielhenriquesouza <gabrielhenriquedeveloper@gmail.com>
2023-09-04 10:42:00 -0300

    expReg

--- js_5_onebitcode/2_expressoes_regulares/index.js
+++ js_5_onebitcode/2_expressoes_regulares/index.js
@@ -1,6 +1,7 @@
 function phoneNumber(phoneNumberString) {
   const fixedString = phoneNumberString.replace(/[\sa-zA-Z]/g, "");
   this.countryCode = fixedString.match(/(?<=\+)\d{1,3}/)[0];
+  this.ddd = fixedString.match(/(?<=\()(\d+)(?=\))/)[0];
+  this.number = fixedString.match(/(?<=\)).+/)[0].replace(/-/g, "");
 }
-
-console.log(phoneNumber("+55 (61) 9 9910-9836"));
+console.table(new phoneNumber("+55 (61) 9 9910-9836"));
